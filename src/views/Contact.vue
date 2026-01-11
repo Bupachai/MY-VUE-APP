0ae2a91fcddae3e0bf3b889bc298e1d24ba09257
Bupachai <tis.wongbua@gmail.com>
2026-01-11 16:53:18 +0700

    update v1

---
+++ src/views/Contact.vue
@@ -0,0 +1,9 @@
+<template lang="">
+    <div>
+        <h1>Contact to Bupachai Wongbua</h1>
+        <h3>Tel.081-148-1631</h3>
+        <img alt="Vue logo" src="../assets/megalodon.png">
+    </div>
+</template>
+
+
