bc66f585098de7bffe6dab40779f2b4c24155692
Bupachai <tis.wongbua@gmail.com>
2026-01-11 18:38:24 +0700

    update v1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,48 @@
 <template>
+<div>
+  <nav class="navbar navbar-expand-lg bg-primary">
+  <div class="container">
+    <a class="navbar-brand" href="/">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/about">About</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="/contact">Contact</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+</div>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
     <router-link to="/contact">Contact</router-link>
   </nav>
   <router-view/>
@@ -13,7 +54,7 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #68d600;
 }
 
 nav {
@@ -22,10 +63,10 @@ nav {
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #26b901;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #00ec82;
 }
 </style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,9 +1,72 @@
-<template lang="">
-    <div>
+<template >
+    <div class= "container" >
         <h1>Contact to Bupachai Wongbua</h1>
-        <h3>Tel.081-148-1631</h3>
+        <h3>tel 081-148-1631</h3>
         <img alt="Vue logo" src="../assets/megalodon.png">
+
+
+<div class="container text-center">
+  <div class="row">
+    <div class="col-sm-6">
     </div>
+    <div class="col-sm-6">
+    </div>
+  </div>
+</div>
+
+  <div class="mb-3">
+    
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+<button type="button" class="btn btn-primary">Submit</button>
+<button type="button" class="btn btn-danger">Cancel</button>
+    </div>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
 </template>
 
+<style scoped>
+    h1{
+        color: aquamarine;
+    }
+    
+
+</style>
 
